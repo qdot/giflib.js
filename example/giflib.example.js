2216c5dcedca39aa96c31564dff7016b6d671e18
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-04 19:02:06 -0700

    Add timing, error printing and close function to example

--- example/giflib.example.js
+++ example/giflib.example.js
@@ -12,13 +12,19 @@ $(document).ready(function() {
     if (document.getElementById('fileinput').files.length == 0) {
       return;
     }
-
+    var start = new Date();
     var g = loadGifFile(document.getElementById('fileinput').files[0]);
+
     var gifLoaded = function (gif) {
       console.log(gif);
+      var finish = new Date();
+      var difference = new Date();
+      difference.setTime(finish.getTime() - start.getTime());
+      console.log("TIME: " + difference.getMilliseconds() );
+      closeGifFile(gif);
     };
-    var gifLoadFailed = function (gif) {
-      console.log("Load failure!");
+    var gifLoadFailed = function (error) {
+      console.log("Load failure! Error Code: " + error);
     };
     g.then(gifLoaded, gifLoadFailed);
   });
