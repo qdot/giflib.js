64137b916cd125b99cdfc197331bbd636636d165
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-04 19:01:50 -0700

    Add more memory and functions to build file

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 . ./config.sh
 
 cd $BUILD_DIR && $EMSCRIPTEN_DIR/emmake make
-cd $BUILD_DIR && $EMSCRIPTEN_DIR/emcc -O2 lib/*.o ../src/giflib.js.c -o $BUILD_DIR/giflib.emscripten.js -I$GIFLIB_DIR/lib -s EXPORTED_FUNCTIONS="['_DGifOpenJS', '_infunc', '_DGifOpen', '_DGifSlurp', '_printStruct']" -s ASSERTIONS=1 -profiling
+cd $BUILD_DIR && $EMSCRIPTEN_DIR/emcc -O3 lib/*.o ../src/giflib.js.c -o $BUILD_DIR/giflib.emscripten.js -I$GIFLIB_DIR/lib -s EXPORTED_FUNCTIONS="['_DGifOpenJS', '_infunc', '_DGifOpen', '_DGifSlurp', '_DGifCloseFile']" -s TOTAL_MEMORY=32000000 -s ASSERTIONS=1
 cat $BUILD_DIR/giflib.emscripten.js $BUILD_DIR/../js/giflib.library.js > $BUILD_DIR/giflib.js
