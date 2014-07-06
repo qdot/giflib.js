10aec18d059217144abe4481eef1670d1d9003a1
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-06 00:01:52 -0700

    Change profile measurements to use console.time, which actually works

--- example/giflib.example.js
+++ example/giflib.example.js
@@ -16,36 +16,22 @@ $(document).ready(function() {
     var g = loadGifFile(document.getElementById('fileinput').files[0]);
 
     var gifLoaded = function (gif) {
-      console.log(gif);
-      var finish = new Date();
-      var difference = new Date();
-      difference.setTime(finish.getTime() - start.getTime());
-      console.log("TIME: " + difference.getMilliseconds() );
-
-      start = new Date();
       updateFrame(gif);
-      finish = new Date();
-      difference = new Date();
-      difference.setTime(finish.getTime() - start.getTime());
-      console.log("TIME: " + difference.getMilliseconds() );
-      console.log("Finished");
+      console.time("loading");
     };
     var imgIdx = 0;
 
     var updateFrame = function (gif) {
+
       var tmpCanvas = document.getElementById('gifcanvas');
-      start = new Date();
       copyImageToCanvas(gif, imgIdx, tmpCanvas);
       imgIdx = imgIdx + 1;
       if(imgIdx < gif.imageCount) {
         setTimeout(updateFrame.bind(undefined, gif), 0);
       } else {
         closeGifFile(gif);
+        console.timeEnd("loading");
       }
-      var finish = new Date();
-      var difference = new Date();
-      difference.setTime(finish.getTime() - start.getTime());
-      console.log("Frame TIME: " + difference.getMilliseconds() );
     };
 
     var gifLoadFailed = function (error) {
